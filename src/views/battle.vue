933f88bbfdadf1aae7a60aac7a2e5eb94988f0d0
nasubi916 <r916nis@gmail.com>
2023-04-13 21:00:58 +0900

    deploy test7

---
+++ src/views/battle.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { updatePlayerName } from '@/server/usePlayerID';
+import { usePlayerStore } from '@/store';
+
+const playerStore = usePlayerStore();
+
+</script>
+
+<template>
+	<div class="flex flex-col items-center justify-center h-screen">
+		<h1 class="text-4xl font-bold mb-4">Home</h1>
+
+	</div>
+</template>
