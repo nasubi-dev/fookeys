0081528aff978572857362d5c603fcf20b30f37b
nasubi916 <r916nis@gmail.com>
2023-04-13 21:15:45 +0900

    vue-routerのせい？

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/index.vue";
 import Menu from "../views/menu.vue";
 import Character from "../views/character.vue";
 import Gift from "../views/gift.vue";
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
