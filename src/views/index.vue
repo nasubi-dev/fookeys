6a4915dae9fd714ab51c7052fb717c56f57a8d03
nasubi916 <r916nis@gmail.com>
2023-04-14 18:57:04 +0900

    コレなら行けるでしょ

--- src/views/index.vue
+++ src/views/index.vue
@@ -20,29 +20,6 @@ async function updateName() {
 	<div class="flex flex-col items-center justify-center h-screen">
 		<h1 class="text-4xl font-bold mb-4">Home</h1>
 
-		<form class="flex flex-col items-center" @submit.prevent="updateName">
-			<label class="mb-2">
-				<input class="border border-gray-400 rounded-lg p-2 w-64" type="text" placeholder="please name"
-					v-model="playerStore.name" />
-			</label>
-			<button class="bg-blue-500 hover:bg-blue-600 text-white font-bold rounded-lg px-4 py-2 btn-pop" type="submit">
-				Rename
-			</button>
-		</form>
 
-		<div class="mt-4">
-			<div>
-				<span class="text-xl font-bold">Your ID:</span>
-				<span class="text-xl">{{ playerStore.id }}</span>
-			</div>
-			<div>
-				<span class="text-xl font-bold">Your Name:</span>
-				<span class="text-xl">{{ newName }}</span>
-			</div>
-		</div>
-
-		<router-link to="/menu" class="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold rounded-lg px-4 py-2 btn-pop">
-			<button> Menu</button>
-		</router-link>
 	</div>
 </template>
