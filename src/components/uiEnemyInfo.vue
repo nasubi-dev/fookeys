1d2e4fa671fb0e030ef1e1fdb2b67f209bc0e34b
nasubi916 <r916nis@gmail.com>
2023-10-26 11:13:41 +0900

    fix(4)

--- src/components/uiEnemyInfo.vue
+++ src/components/uiEnemyInfo.vue
@@ -38,7 +38,7 @@ defineProps<{
         </div>
 
         <div>
-          <UiGifts :gifts="p.gifts" :p="p" class="transform -translate-x-10 -translate-y-8" />
+          <UiGifts :gifts="p.gifts" :p="p" class="transform -translate-x-10 -translate-y-4" />
         </div>
       </div>
     </div>
