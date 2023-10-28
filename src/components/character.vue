7a9c7ecfb65f87083238e51629cb5d8815d484ba
nasubi916 <r916nis@gmail.com>
2023-10-28 14:15:09 +0900

    Def表示最適完了!

--- src/components/character.vue
+++ src/components/character.vue
@@ -11,8 +11,6 @@ const { game } = storeToRefs(gameStore);
 const { firstAtkPlayer } = toRefs(game.value)
 
 const characterName = ref("blankiss")
-const retainedDef = ref<number>(0);
-const reactionImg = ref<string>("normal");
 
 onMounted(async () => {
   await wait(1)
@@ -20,47 +18,34 @@ onMounted(async () => {
   characterName.value = characterAllocation ? "blankiss" : "petit&spot"
 })
 
+const retainedDef = ref<number>(0);
+const reactionImg = ref<string>("normal");
 watch(battleResult, (newVal) => {
   if (typeof newVal[1] !== "number") return
-
   const isPrimaryAtk = components.value === "primaryAtk"
   const changingValueAllocation = !XOR(firstAtkPlayer.value === sign.value, p.status === "my")// X-NOR
-  if (components.value === "primaryAtk") {
-    if (changingValueAllocation) {
-      if (newVal[0] === "def") {
-        reactionImg.value = "def"
-        retainedDef.value = newVal[1]
-      } else if (newVal[0] === "atk") {
-        reactionImg.value = "atk"
-      }//!techやsupのときも追加する
-    } else {
-      if (newVal[0] === "atk") {
-        reactionImg.value = "damage"
-        if (retainedDef.value < 0) retainedDef.value = 0
-      } else if (newVal[0] === "tech") {
-        reactionImg.value = "damage"
-      }
+  const attackOrderAllocation = !XOR(isPrimaryAtk, changingValueAllocation)// X-NOR
+
+  if (attackOrderAllocation) {
+    if (newVal[0] === "def") {
+      reactionImg.value = "def"
+      retainedDef.value = newVal[1]
+    } else if (newVal[0] === "atk" || newVal[0] === "tech") {
+      reactionImg.value = "atk"
     }
-  } else if (components.value === "secondAtk") {
-    if (!changingValueAllocation) {
-      if (newVal[0] === "def") {
-        reactionImg.value = "def"
-        retainedDef.value = newVal[1]
-      } else if (newVal[0] === "atk") {
-        reactionImg.value = "atk"
-      }
-    } else {
-      if (newVal[0] === "atk") {
-        reactionImg.value = "damage"
-        retainedDef.value -= newVal[1]
-        if (retainedDef.value < 0) retainedDef.value = 0
-      } else if (newVal[0] === "tech") {
-        reactionImg.value = "damage"
-      }
+  } else {
+    if (newVal[0] === "atk") {
+      reactionImg.value = "damage"
+      retainedDef.value -= isPrimaryAtk ? 0 : newVal[1]
+      if (retainedDef.value < 0) retainedDef.value = 0
     }
-    if (newVal[0] === "none") retainedDef.value = 0
   }
-  if (newVal[0] === "none") reactionImg.value = "normal"
+
+  //初期化
+  if (newVal[0] === "none") {
+    if (!isPrimaryAtk) retainedDef.value = 0
+    reactionImg.value = "normal"
+  }
 })
 </script>
 
