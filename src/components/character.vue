f8c9b8c0bef8ba04bf5d3c23a2eb1e91cd2f835e
nasubi916 <r916nis@gmail.com>
2023-10-28 13:37:31 +0900

    条件分岐を切り分けて簡略化

--- src/components/character.vue
+++ src/components/character.vue
@@ -1,90 +1,66 @@
 <script setup lang="ts">
 import { ref, toRefs, watch, onMounted } from "vue";
-import { playerStore, enemyPlayerStore, gameStore } from "@/main";
+import { playerStore, gameStore } from "@/main";
 import { wait, XOR } from "@/server/utils";
 import { storeToRefs } from "pinia";
 
 const p = defineProps<{ status: "my" | "enemy" }>()
 
-const { player, components, battleResult, sign } = storeToRefs(playerStore);
-const { sumFields } = toRefs(player.value);
-const { enemyPlayer } = storeToRefs(enemyPlayerStore);
+const { components, battleResult, sign } = storeToRefs(playerStore);
 const { game } = storeToRefs(gameStore);
 const { firstAtkPlayer } = toRefs(game.value)
 
 const characterName = ref("blankiss")
-let a: boolean
-onMounted(async () => {
-  await wait(1)
-  a = !XOR(p.status === "my", sign.value === 0)// X-NOR
-  characterName.value = a ? "blankiss" : "petit&spot"
-})
-
 const retainedDef = ref<number>(0);
 const reactionImg = ref<string>("normal");
+
+onMounted(async () => {
+  await wait(1)
+  const characterAllocation = !XOR(p.status === "my", sign.value === 0)// X-NOR
+  characterName.value = characterAllocation ? "blankiss" : "petit&spot"
+})
+
 watch(battleResult, (newVal) => {
-  const c = firstAtkPlayer.value === sign.value
-  // const d = XOR(c, p.status === "my")
-  if (!(typeof newVal[1] === "number")) return
+  if (typeof newVal[1] !== "number") return
+
+  const isPrimaryAtk = components.value === "primaryAtk"
+  const changingValueAllocation = !XOR(firstAtkPlayer.value === sign.value, p.status === "my")// X-NOR
   if (components.value === "primaryAtk") {
-    if (c && p.status === "my") {
+    if (changingValueAllocation) {
       if (newVal[0] === "def") {
         reactionImg.value = "def"
         retainedDef.value = newVal[1]
       } else if (newVal[0] === "atk") {
         reactionImg.value = "atk"
-      }
-    } else if (!c && p.status === "enemy") {
-      if (newVal[0] === "def") {
-        reactionImg.value = "def"
-        retainedDef.value = newVal[1]
-      } else if (newVal[0] === "atk") {
-        reactionImg.value = "atk"
-      }
-    } else if (!c && p.status === "my") {
+      }//!techやsupのときも追加する
+    } else {
       if (newVal[0] === "atk") {
         reactionImg.value = "damage"
         if (retainedDef.value < 0) retainedDef.value = 0
-      }
-    } else if (c && p.status === "enemy") {
-      if (newVal[0] === "atk") {
+      } else if (newVal[0] === "tech") {
         reactionImg.value = "damage"
-        if (retainedDef.value < 0) retainedDef.value = 0
       }
     }
   } else if (components.value === "secondAtk") {
-    if (!c && p.status === "my") {
+    if (!changingValueAllocation) {
       if (newVal[0] === "def") {
         reactionImg.value = "def"
         retainedDef.value = newVal[1]
       } else if (newVal[0] === "atk") {
         reactionImg.value = "atk"
       }
-    } else if (c && p.status === "enemy") {
-      if (newVal[0] === "def") {
-        reactionImg.value = "def"
-        retainedDef.value = newVal[1]
-      } else if (newVal[0] === "atk") {
-        reactionImg.value = "atk"
-      }
-    } else if (c && p.status === "my") {
+    } else {
       if (newVal[0] === "atk") {
         reactionImg.value = "damage"
         retainedDef.value -= newVal[1]
         if (retainedDef.value < 0) retainedDef.value = 0
-      }
-    } else if (!c && p.status === "enemy") {
-      if (newVal[0] === "atk") {
+      } else if (newVal[0] === "tech") {
         reactionImg.value = "damage"
-        retainedDef.value -= newVal[1]
-        if (retainedDef.value < 0) retainedDef.value = 0
       }
     }
+    if (newVal[0] === "none") retainedDef.value = 0
   }
-})
-watch(components, (newVal) => {
-  if (newVal === "postBattle") retainedDef.value = 0
-  reactionImg.value = "normal"
+  if (newVal[0] === "none") reactionImg.value = "normal"
 })
 </script>
 
