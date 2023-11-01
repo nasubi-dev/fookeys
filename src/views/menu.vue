f29d2f1f48b870c3c336e51ffe4fdcc0d5e3f9a9
nasubi916 <r916nis@gmail.com>
2023-11-01 17:31:53 +0900

    menuに画像を追加

--- src/views/menu.vue
+++ src/views/menu.vue
@@ -6,55 +6,60 @@ import { playerStore } from "@/main";
 import { useSound } from "@vueuse/sound";
 import { tap1, tap2 } from "@/assets/sounds";
 import { startMatchmaking } from "@/server/useMatchMaking";
+//components
 import SelectCharacter from "@/components/selectCharacter.vue";
 import SelectGifts from "@/components/selectGifts.vue";
-import nasubi from "@/assets/img/nasubi.png";
+import UiGifts from "@/components/uiGifts.vue";
+//img
+import characterBackground from "@/assets/img/ui/characterBackground.png";
+import menuBackground from "@/assets/img/ui/menuBackground.png";
+import back from "@/assets/img/ui/back.png";
 
 import { usePush } from 'notivue'
 const push = usePush()
 
-const { player, log } = storeToRefs(playerStore);
+const { player, log, id } = storeToRefs(playerStore);
 const { gifts, character } = toRefs(player.value);
+
+watch(log, (newVal) => {
+  if (newVal) push.info(newVal)
+})
 
 const useTap1 = useSound(tap1);
 const useTap2 = useSound(tap2);
 
 const selectGift = ref(false);
 const selectCharacter = ref(false);
-
-//マッチングを開始する
-//マッチングが成功したら後で押したほうがPlayer1､Player2
 async function startMatch(): Promise<void> {
+  if (!id.value) {
+    push.error("IDがありません｡")
+    return
+  }
   await startMatchmaking();
 }
-
-watch(log, (newVal) => {
-  if (newVal) push.info(newVal)
-})
 </script>
 
 <template>
   <div class="h-screen flex flex-col">
-    <router-link v-if="!selectCharacter && !selectGift" to="/">
-      <button @click="useTap2.play()"
-        class="p-4 absolute top-4 left-4 bg-blue-500 hover:bg-blue-600 text-white rounded-md btn-pop">
-        戻る
-      </button>
-    </router-link>
-    <div v-else>
-      <button @click="selectCharacter = false; selectGift = false; useTap2.play()"
-        class="p-4 absolute top-4 left-4 bg-blue-500 hover:bg-blue-600 text-white rounded-md btn-pop">
-        メニューへ戻る
+    <div class="z-10">
+      <router-link v-if="!selectCharacter && !selectGift" to="/">
+        <button @click="useTap2.play()" class="p-4 absolute top-4 left-4 btn-pop">
+          <img :src="back" class="w-32" />
+        </button>
+      </router-link>
+      <button v-else class="p-4 absolute top-4 left-4 btn-pop"
+        @click="selectCharacter = false; selectGift = false; useTap2.play()">
+        <img :src="back" class="w-32" />
       </button>
     </div>
 
     <div class="flex flex-1">
-      <div class="w-1/2 p-8 flex flex-col justify-center text-center items-center">
-        <img :src="nasubi" class="w-1/2" />
-        se-sakusya
-        <div class="flex justify-start w-1/2">
-          <div v-for=" gift  in  gifts " :key="gift">
-            <img :src="`./img/gifts/${gift}.png`" />
+      <div class="overCard w-1/2 p-8 flex flex-col justify-center items-center text-center">
+        <img :src="characterBackground" />
+        <div class="overText items-center ">
+          <img :src="`/img/characters/${character}/normal.png`" class="w-1/2" />
+          <div class="flex justify-start w-1/2">
+            <UiGifts :gifts="gifts" :player="player" />
           </div>
         </div>
       </div>
@@ -62,13 +67,16 @@ watch(log, (newVal) => {
       <div class="w-1/2 flex flex-col justify-center text-center items-center">
         <SelectCharacter v-if="selectCharacter" />
         <SelectGifts v-else-if="selectGift" />
-        <div v-else class="w-1/2">
-          <button @click="startMatch(); useTap1.play()" class="btn-pop">
-            <img src="@/assets/img/ui/entry.png" />
-          </button>
-          <button @click="selectGift = !selectGift; useTap1.play()" class="btn-pop">
-            <img src="@/assets/img/ui/changeGift.png" />
-          </button>
+        <div v-else class="overCard ">
+          <img :src="menuBackground" class="h-full" />
+          <div class="overText">
+            <button @click="startMatch(); useTap1.play()" class="btn-pop">
+              <img src="@/assets/img/ui/entry.png" />
+            </button>
+            <button @click="selectGift = !selectGift; useTap1.play()" class="btn-pop">
+              <img src="@/assets/img/ui/changeGift.png" />
+            </button>
+          </div>
         </div>
       </div>
     </div>
