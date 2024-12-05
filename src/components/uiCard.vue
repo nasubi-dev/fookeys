9823f0c9986e0f010ab5a7aa4a4e41b04c1b34d9
nasubi916 <r916nis@gmail.com>
2024-12-05 10:23:46 +0900

    ショップの特殊効果タグを大きく

--- src/components/uiCard.vue
+++ src/components/uiCard.vue
@@ -61,7 +61,7 @@ const onKeyUpCallbackHook = (): void => {
           </div>
         </div>
         <div v-if="card.description && card.id !== 0" class="absolute"
-          :class="size === 'normal' ? `top-5 right-5 w-[max(2vw,30px)]` : `top-7 right-8 w-8`">
+          :class="size === 'normal' ? `top-5 right-5 w-[max(2vw,30px)]` : `top-7 right-8 w-[max(2vw,50px)]`">
           <img :src="`/img/showSpecial/${card.company}.png`" class="absolute" />
         </div>
       </VDuringPress>
