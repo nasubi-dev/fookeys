f87cd0522f49bedb306b8ebd11a0d9aebb35c6d4
nasubi916 <r916nis@gmail.com>
2024-12-11 11:53:00 +0900

    カードの重ね順修正

--- src/components/uiCard.vue
+++ src/components/uiCard.vue
@@ -27,11 +27,8 @@ const onKeyUpCallbackHook = (): void => {
 
 <template>
   <div class="min-w-[7rem]">
-    <div
-      v-if="dropDown"
-      class="w-[max(20vw,340px)] z-20 mx-3 fixed text-gray-900 text-left"
-      :class="[card.description ? ' -translate-y-20  z-20' : '-translate-y-10', state ? `` : `max-w-fit`]"
-    >
+    <div v-if="dropDown" class="w-[max(20vw,340px)] z-20 mx-3 fixed text-gray-900 text-left -translate-y-28"
+      :class="[state ? `` : `max-w-fit`]">
       <div class="absolute w-[max(20vw,340px)]">
         <img :src="bg" class="z-20 absolute" />
         <div class="z-20 p-4 px-5 absolute">
@@ -44,33 +41,21 @@ const onKeyUpCallbackHook = (): void => {
       <VDuringPress :onKeyDown="onLongPressCallbackHook" :onKeyUp="onKeyUpCallbackHook" :delay="250">
         <img :src="`/img/companys/${card.company}.png`" class="min-w-[7rem]" />
         <div class="overText min-w-[7rem]">
-          <p
-            v-if="card.waste"
-            class="font-bold text-center transform select-none"
-            :class="[
-              size === 'normal'
-                ? `text-[max(2vw,1rem)] -translate-x-[max(2.6vw,210%)] translate-y-[min(2.2vw,500px)] `
-                : `text-[max(2vw,1rem)] -translate-x-[280%] translate-y-[140%]`,
-              card.waste === 1 ? `-translate-x-[380%]` : null,
-            ]"
-          >
+          <p v-if="card.waste" class="font-bold text-center transform select-none" :class="[
+            size === 'normal'
+              ? `text-[max(2vw,1rem)] -translate-x-[max(2.6vw,210%)] translate-y-[min(2.2vw,500px)] `
+              : `text-[max(2vw,1rem)] -translate-x-[280%] translate-y-[140%]`,
+            card.waste === 1 ? `-translate-x-[380%]` : null,
+          ]">
             {{ card.waste }}
           </p>
-          <img
-            v-if="card.waste"
-            :src="`/img/foods/${card.id}.png`"
-            class="transform"
-            :class="size === 'normal' ? `-translate-x-[10%]` : `-translate-x-[10%] translate-y-[10%]`"
-          />
+          <img v-if="card.waste" :src="`/img/foods/${card.id}.png`" class="transform"
+            :class="size === 'normal' ? `-translate-x-[10%]` : `-translate-x-[10%] translate-y-[10%]`" />
 
-          <div
-            class="flex font-black text-border-thin transform select-none"
-            :class="
-              size === 'normal'
-                ? `text-[max(1vw,0.5rem)] -translate-x-[10%]  -translate-y-[60%]`
-                : `text-lg -translate-x-[10%] -translate-y-[40%]`
-            "
-          >
+          <div class="flex font-black text-border-thin transform select-none" :class="size === 'normal'
+              ? `text-[max(1vw,0.5rem)] -translate-x-[10%]  -translate-y-[60%]`
+              : `text-lg -translate-x-[10%] -translate-y-[40%]`
+            ">
             <p v-if="card.hungry !== undefined && card.id !== 0">{{ "🍖" + card.hungry }}</p>
             <p v-if="card.atk">{{ "💪" + card.atk }}</p>
             <p v-if="card.def">{{ "🛡" + card.def }}</p>
@@ -78,11 +63,8 @@ const onKeyUpCallbackHook = (): void => {
             <p v-if="card.heal">{{ "💖" + card.heal }}</p>
           </div>
         </div>
-        <div
-          v-if="card.description && card.id !== 0"
-          class="absolute"
-          :class="size === 'normal' ? `top-5 right-5 w-[max(2vw,30px)]` : `top-7 right-8 w-[max(2vw,50px)]`"
-        >
+        <div v-if="card.description && card.id !== 0" class="absolute"
+          :class="size === 'normal' ? `top-5 right-5 w-[max(2vw,30px)]` : `top-7 right-8 w-[max(2vw,50px)]`">
           <img :src="`/img/showSpecial/${card.company}.png`" class="absolute" />
         </div>
       </VDuringPress>
